9d379e5f63b7ba8ec23e6ee61a3d55aad52dc14c
unknown <wolkdem@gmail.com>
2024-02-16 11:04:41 +0200

    (Transition): completely finish with transition to tsx

---
+++ src/components/Player.tsx
@@ -0,0 +1,38 @@
+import { useState } from "react"
+
+interface Props {
+  initialName: string
+  symbol: string
+  isActive: boolean
+  onChangeName: (symbol: string, newName: string) => void
+}
+export default function Player({ initialName, symbol, isActive, onChangeName }: Props) {
+  const [name, setName] = useState(initialName)
+  const [edit, isEditing] = useState(false)
+
+  function handleEditUser() {
+    isEditing((edit) => !edit)
+    if (edit) {
+      onChangeName(symbol, name)
+    }
+  }
+
+  function handleNameChange(event: any) {
+    setName(event.target.value)
+  }
+  let playerName = <span className="player-name">{name}</span>
+  let btnCaption = "Edit"
+  if (edit) {
+    playerName = <input type="text" required value={name} onChange={handleNameChange} />
+    btnCaption = "Save"
+  }
+  return (
+    <li className={isActive ? "active" : undefined}>
+      <span className="player">
+        {playerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditUser}>{edit ? "Save" : "Edit"}</button>
+    </li>
+  )
+}
